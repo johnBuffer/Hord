88f3a65197b5baa56e26ecc58d096dd7d31e7f78
Jean Tampon <jean.tampon@gmail.com>
2020-07-22 16:49:39 +0200

    Trying something

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "display_manager.hpp"
 #include "grid.hpp"
 #include "render.hpp"
+#include "agent.hpp"
 
 
 int main()
@@ -23,6 +24,12 @@ int main()
     Grid grid(20, 80, 45);
     sf::Vector2f start(0.0f, 0.0f);
     sf::Vector2f end(10.0f, 10.0f);
+
+    const uint32_t agents_count = 10;
+    std::vector<Agent> agents;
+    for (uint32_t i(agents_count); i--;) {
+        agents.emplace_back(rand()%WinWidth, rand() % WinHeight);
+    }
 
 	DisplayManager display_manager(window);
     display_manager.event_manager.addKeyPressedCallback(sf::Keyboard::C, [&](const sf::Event& ev) {
@@ -44,12 +51,18 @@ int main()
 
     while (window.isOpen()) {
         display_manager.processEvents();
+        const sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+
+        for (Agent& a : agents) {
+            a.update(sf::Vector2f(mouse_pos.x, mouse_pos.y), grid, 0.016f);
+        }
 
         window.clear(sf::Color::Black);
 
-        Renderer::renderGrid(window, grid, display_manager.getRenderStates());
+        const sf::RenderStates rs = display_manager.getRenderStates();
+        Renderer::renderGrid(window, grid, rs);
 
-        const float r = 8.0f;
+       /* const float r = 8.0f;
         sf::CircleShape c(r);
         c.setOrigin(r, r);
         c.setPosition(start);
@@ -66,7 +79,11 @@ int main()
         sight_va[1].position = end;
         sight_va[0].color = sf::Color::Red;
         sight_va[1].color = sf::Color::Red;
-        window.draw(sight_va);
+        window.draw(sight_va);*/
+
+        for (const Agent& a : agents) {
+            a.draw(window, rs);
+        }
 
 		window.display();
     }
